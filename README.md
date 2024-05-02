15537ae64d7afad54d45c6e71c2d1767674d01c4
dimkanividimka <velo.dimon.96@gmail.com>
2024-05-02 12:40:53 +0200

    Command to create stored procedures were added

--- README.md
+++ README.md
@@ -8,5 +8,6 @@ Some details and features:
  - TemperaturesConditions table is a conformed dimension table, used by Shippers and Products tables.
  - Customers table implemented as a slowly changing dimension table (Type 2).
  - Aside from standard MySQL indexes, it has B-Tree indexed City column in tables Customers and Suppliers.
+ - Stored procedures to find products with prices above specified threshold and to find top customers with sales amount above specified threshold.
     
 ![Schema](https://github.com/dimkanividimka/ChemdoorDB/blob/main/Schema.PNG)
---
+++ StoredProcedures.sql
@@ -0,0 +1,28 @@
+DELIMITER $$
+
+-- THE FOLLOWING PROCEDURE WILL RETURN PRODUCTS WITH SELLING PRICE ABOVE SPECIFIED --
+CREATE PROCEDURE ProductsWithSellingPriceAboveP(IN P DECIMAL(9,2))
+BEGIN
+	SELECT *
+    FROM products
+    WHERE SellingPrice > P;
+END$$
+
+-- THE FOLLOWING PROCEDURE WILL RETURN CLIENTS WITH TOTAL ORDERS NUMBER ABOVE SPECIFIED --
+CREATE PROCEDURE ClientsWithOrdersNumberAboveO(IN O DECIMAL(9,2))
+BEGIN
+	SELECT c.CustomerName,
+	COUNT(*) AS TotalOrders
+	FROM customers AS c
+	JOIN orders AS o
+		ON c.CustomerTableEntryID = o.CustomerTableEntryID
+	GROUP BY c.CustomerName
+	HAVING TotalOrders > O
+	ORDER BY TotalOrders DESC;
+END$$
+
+DELIMITER ;
+
+	-- Created stored procedures can be then called with the following commands: --
+-- CALL ProductsWithSellingPriceAboveP(200);
+-- CALL ClientsWithOrdersNumberAboveO(5);
